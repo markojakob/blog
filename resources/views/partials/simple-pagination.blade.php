ede332a52a5d5be977085e41e3c832a9c4128700
markojakob <markjakobo@gmail.com>
2025-11-30 12:24:18 +0200

    Added Simple paginate example with DaisyUI

--- resources/views/partials/simple-pagination.blade.php
+++ resources/views/partials/simple-pagination.blade.php
@@ -1,19 +1,25 @@
 @if ($paginator->hasPages())
-    <nav>
-        <ul class="pagination">
-            {{-- Previous Page Link --}}
-            @if ($paginator->onFirstPage())
-                <li class="disabled" aria-disabled="true"><span>@lang('pagination.previous')</span></li>
-            @else
-                <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">@lang('pagination.previous')</a></li>
-            @endif
+    <div class="join">
+        {{-- Previous Page Link --}}
+        @if ($paginator->onFirstPage())
+            <button class="join-item btn btn-outline btn-disabled" disabled>
+                @lang('pagination.previous')
+            </button>
+        @else
+            <a href="{{ $paginator->previousPageUrl() }}" class="join-item btn btn-outline">
+                @lang('pagination.previous')
+            </a>
+        @endif
 
-            {{-- Next Page Link --}}
-            @if ($paginator->hasMorePages())
-                <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')</a></li>
-            @else
-                <li class="disabled" aria-disabled="true"><span>@lang('pagination.next')</span></li>
-            @endif
-        </ul>
-    </nav>
+        {{-- Next Page Link --}}
+        @if ($paginator->hasMorePages())
+            <a href="{{ $paginator->nextPageUrl() }}" class="join-item btn btn-outline">
+                @lang('pagination.next')
+            </a>
+        @else
+            <button class="join-item btn btn-outline btn-disabled" disabled>
+                @lang('pagination.next')
+            </button>
+        @endif
+    </div>
 @endif
